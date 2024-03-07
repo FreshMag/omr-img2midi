7023c2e6c5175cd441fe3ae9f9114654e5024d45
FreshMag <pescomagnani@gmail.com>
2024-03-07 15:53:14 +0100

    Started working on core part

---
+++ src/core/bounding_box.py
@@ -0,0 +1,8 @@
+
+class BoundingBox:
+    def __init__(self, upper_left, lower_right):
+        self.upper_left = upper_left
+        self.lower_right = lower_right
+
+    def apply_on_image(self, image):
+        return image[self.upper_left[0]:self.lower_right[0], self.upper_left[1]:self.lower_right[1]]
