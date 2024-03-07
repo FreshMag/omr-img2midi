e8fd336197ea18d547fcf26f7832523f64c63910
FreshMag <pescomagnani@gmail.com>
2024-03-07 20:57:18 +0100

    Refactored quadrilaterals.py

--- src/core/img2doc/quadrilaterals.py
+++ src/core/img2doc/quadrilaterals.py
@@ -1,12 +1,11 @@
 import math
-import numpy as np
+
 import cv2
-import itertools
-from . import transform
+import numpy as np
 
 
 def is_valid_quadrilateral(quad, image_w, image_h, min_quad_area_ratio, max_quad_angle_range):
-    """Returns True if the contour satisfies all requirements set at instantitation"""
+    """Returns True if the contour satisfies all requirements set at instantiation"""
 
     return (len(quad) == 4 and cv2.contourArea(quad) > image_w * image_h * min_quad_area_ratio
             and angle_range(quad) < max_quad_angle_range)
@@ -18,11 +17,11 @@ def angle_range(quad):
     The input quadrilateral must be a numpy array with vertices ordered clockwise
     starting with the top left vertex.
     """
-    tl, tr, br, bl = quad
-    ura = get_angle(tl[0], tr[0], br[0])
-    ula = get_angle(bl[0], tl[0], tr[0])
-    lra = get_angle(tr[0], br[0], bl[0])
-    lla = get_angle(br[0], bl[0], tl[0])
+    top_left, top_right, bottom_right, bottom_left = quad
+    ura = get_angle(top_left[0], top_right[0], bottom_right[0])
+    ula = get_angle(bottom_left[0], top_left[0], top_right[0])
+    lra = get_angle(top_right[0], bottom_right[0], bottom_left[0])
+    lla = get_angle(bottom_right[0], bottom_left[0], top_left[0])
 
     angles = [ura, ula, lra, lla]
     return np.ptp(angles)
@@ -47,20 +46,6 @@ def get_angle(p1, p2, p3):
     cvec = c - b
 
     return angle_between_vectors_degrees(avec, cvec)
-
-
-def most_probable_quadrilateral(corners):
-    quads = []
-    for quad in itertools.combinations(corners, 4):
-        points = np.array(quad)
-        points = transform.order_points(points)
-        points = np.array([[p] for p in points], dtype="int32")
-        quads.append(points)
-
-    # get top five quadrilaterals by area
-    quads = sorted(quads, key=cv2.contourArea, reverse=True)[:5]
-    # sort candidate quadrilaterals by their angle range, which helps remove outliers
-    return sorted(quads, key=angle_range)[0]
 
 
 def get_max_area_or_whole(contours, image_w, image_h):
