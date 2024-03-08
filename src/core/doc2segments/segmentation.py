848e2753161a1c29f50fb3bdb3660d61a250469e
FreshMag <pescomagnani@gmail.com>
2024-03-08 18:47:39 +0100

    Added function to call clustering algorithms

--- src/core/doc2segments/segmentation.py
+++ src/core/doc2segments/segmentation.py
@@ -1,3 +1,16 @@
+import cv2
+
+from .clustering import cluster_bboxes
+from src.util.imgutils import show_image
+
 
 def segment_doc(doc):
+
+    bboxes = cluster_bboxes(doc)
+
+    img_with_bboxes = cv2.cvtColor(doc.copy(), cv2.COLOR_GRAY2BGR)
+    for bbox in bboxes:
+        bbox.draw_on_image(img_with_bboxes)
+
+    show_image(img_with_bboxes)
     return []
