cb80078c065a93e20d12c53e8ddc233ce9b4556c
FreshMag <pescomagnani@gmail.com>
2024-04-11 10:27:46 +0200

    Added utility function to perform end to end recognition of image

--- src/util/one_pass.py
+++ src/util/one_pass.py
@@ -1,5 +1,4 @@
-from src.semantic.ctc_predict import CTC
-from src.semantic.sheet import EncodedSheet
+from semantic import CTC, EncodedSheet
 
 
 def one_pass_sheet(image_path):
@@ -9,11 +8,27 @@ def one_pass_sheet(image_path):
     return sheet
 
 
+def end2end_recognition(segments, model, vocabulary_path=None):
+    assert model is not None
+    assert segments is not None
+    sheet = EncodedSheet(vocabulary_path)
+    subsheets = []
+
+    for segment in segments:
+        predictions = model.predict(segment)
+        sheet.add_from_predictions(predictions)
+        subsheet = EncodedSheet(vocabulary_path)
+        subsheet.add_from_predictions(predictions)
+        subsheets.append(subsheet)
+    return sheet, subsheets
+
+
 def one_pass_metric(image_path, ground_truth_path):
     sheet = one_pass_sheet(image_path)
     true_sheet = EncodedSheet()
     true_sheet.add_from_semantic_file(ground_truth_path)
     return sheet.compare(true_sheet)
+
 
 if __name__ == '__main__':
     image_path = "../../data/input/test1.png"
