efdb65af230f31961a1776ac54d9a923c707e53c
FreshMag <pescomagnani@gmail.com>
2024-04-24 17:03:55 +0200

    Changed scanner's image thresholding + added utility method to adjust image exposition based on histogram

--- src/core/img2doc/imutils.py
+++ src/core/img2doc/imutils.py
@@ -2,7 +2,6 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from util import show_image
 
 
 def translate(image, x, y):
@@ -29,6 +28,51 @@ def rotate(image, angle, center=None, scale=1.0):
 
     # Return the rotated image
     return rotated
+
+
+# Automatic brightness and contrast optimization with optional histogram clipping
+def automatic_brightness_and_contrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = [float(hist[0])]
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index - 1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum / 100.0)
+    clip_hist_percent /= 2.0
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size - 1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
+    plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return auto_result, alpha, beta
 
 
 def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -62,20 +106,22 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     return resized
 
 
-def warped2sharpened(image, threshold_constant=15):
+def warped2sharpened(image, thresh_block_size, thresh_c):
+    img = automatic_brightness_and_contrast(image)[0]
     # Convert the warped image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Sharpen the image using un-sharp masking
-    #sharpen = cv2.GaussianBlur(gray, blur_kernel_size, 0)
-    #sharpen = cv2.addWeighted(gray, sharpen_weight, sharpen, sharpen_blur_weight, 0)
-    blurred = cv2.GaussianBlur(gray, (1, 1), 0.2)
-    thresh = cv2.threshold(blurred, threshold_constant, 255, cv2.THRESH_OTSU)[1]
-    # Apply adaptive thresholding to get black and white effect
-    #thresh = cv2.adaptiveThreshold(sharpen, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
-    #                               threshold_block_size, threshold_constant)
-    #k = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
-    #closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, k)
+    # sharpen image
+    sharpen = cv2.GaussianBlur(gray, (0, 0), 3)
+    sharpen = cv2.addWeighted(gray, 1.5, sharpen, -0.5, 0)
+
+    # apply adaptive threshold to get black and white effect
+    thresh = cv2.adaptiveThreshold(sharpen, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
+                                   thresh_block_size, thresh_c)
+
+
+    # k = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+    # thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, k, iterations=close_iterations)
 
     return thresh
 
